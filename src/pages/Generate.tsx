e736ce836dec80abd7dba076e9cc426dfee889e7
Tanzil khan <khantanzil383@gmail.com>
2026-06-11 11:05:16 +0500

    Add SoftBackdrop to Generate page

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,24 +1,56 @@
-import { useParams } from "react-router-dom"
-import SoftBackdrop from "../components/SoftBackdrop"
+import { useParams } from "react-router-dom";
+import SoftBackdrop from "../components/SoftBackdrop";
 import { useState } from "react";
 import type { IThumbnail } from "../assets/assets";
 
 const Generate = () => {
-  const {id} = useParams();
-  const [title, setTitle] = useState('')
-  const [additional, setadditionalDetails] = useState('')
-  const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null)
-  const [loading, setLoading] = useState(false)
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [additional, setadditionalDetails] = useState("");
+  const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null);
+  const [loading, setLoading] = useState(false);
 
   return (
-   <>
-   <SoftBackdrop/>
-   <div className="pt-24 min-h-screen">
+    <>
+      <SoftBackdrop />
+      <div className="pt-24 min-h-screen">
+        <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-28 lg:pb-8">
+          <div className="grid lg:grid-cols-[400px_1fr] gap-8">
+            {/* {LEFT PANEL} */}
+            <div className={`space-y-6 ${id && "pointer-events-none"}`}>
+              <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
+                <div>
+                  <h2 className="text-xl font-bold text-zinc-100 mb-1">
+                    Create Your Thumbnail
+                  </h2>
+                  <p className="text-sm text-zinc-400 ">
+                    Describe your vision and let AI bring it to life
+                  </p>
+                </div>
+                <div className="space-y-5"></div>
+                {/* {Button} */}
+                {!id && (
+                  <button
+                    className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600
+                  hover:from-pink-700 disabled:cursor-not-allowed transition-colors"
+                  >
+                    {loading
+                      ? `<button disabled className="flex items-center justify-center gap-2 px-4 py-2 bg-black text-white rounded-md">
+  <span className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin"></span>
+  Loading...
+</button>`
+                      : `Generate Thumbnail`}
+                  </button>
+                )}
+              </div>
+            </div>
+            {/* {RIGHT PANEL} */}
+            <div></div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
 
-   </div>
-
-   </>
-  )
-}
-
-export default Generate
+export default Generate;
