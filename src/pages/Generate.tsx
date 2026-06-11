791606b84d9076fb065e7542552b3af53555bdb0
Tanzil khan <khantanzil383@gmail.com>
2026-06-11 10:05:28 +0500

    Generate page bg color update

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,9 +1,22 @@
+import { useParams } from "react-router-dom"
 import SoftBackdrop from "../components/SoftBackdrop"
+import { useState } from "react";
+import type { IThumbnail } from "../assets/assets";
 
 const Generate = () => {
+  const {id} = useParams();
+  const [title, setTitle] = useState('')
+  const [additional, setadditionalDetails] = useState('')
+  const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null)
+  const [loading, setLoading] = useState(false)
+
   return (
    <>
    <SoftBackdrop/>
+   <div className="pt-24 min-h-screen">
+
+   </div>
+
    </>
   )
 }
